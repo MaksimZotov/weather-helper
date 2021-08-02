aa16c5ae802a5f37b9447f143cc71a514ac32d9b
MaksimZotov <zotovmaksim1254@gmail.com>
2021-08-02 20:58:59 +0300

    made the basis for working with the common toolbar

--- app/src/main/java/com/maksimzotov/weatherhelper/presentation/ui/cities/CitiesFragment.kt
+++ app/src/main/java/com/maksimzotov/weatherhelper/presentation/ui/cities/CitiesFragment.kt
@@ -5,6 +5,8 @@ import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.*
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -18,13 +20,20 @@ import com.maksimzotov.weatherhelper.presentation.main.listeners.OnItemClickList
 import com.maksimzotov.weatherhelper.presentation.main.ui.BaseFragment
 import com.maksimzotov.weatherhelper.presentation.ui.cities.recyclerview.CitiesAdapter
 
+
 class CitiesFragment :
     BaseFragment<CitiesFragmentBinding>(CitiesFragmentBinding::inflate),
+    SearchView.OnQueryTextListener,
     OnItemClickListener {
 
     private val viewModel by viewModels<CitiesViewModel>()
     private lateinit var citiesAdapter: CitiesAdapter
     private var prevBG: Drawable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -106,7 +115,26 @@ class CitiesFragment :
         }).attachToRecyclerView(recyclerView)
     }
 
+
     override fun onItemClick(position: Int) {
         findNavController().navigate(R.id.cityFragment)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+
+        val menuItem = menu.findItem(R.id.menu_search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Toast.makeText(requireContext(), "Submit", Toast.LENGTH_SHORT).show()
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Toast.makeText(requireContext(), "Change", Toast.LENGTH_SHORT).show()
+        return true
     }
 }
--- app/src/main/java/com/maksimzotov/weatherhelper/presentation/ui/city/CityFragment.kt
+++ app/src/main/java/com/maksimzotov/weatherhelper/presentation/ui/city/CityFragment.kt
@@ -1,9 +1,14 @@
 package com.maksimzotov.weatherhelper.presentation.ui.city
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.google.android.material.tabs.TabLayoutMediator
+import com.maksimzotov.weatherhelper.R
 import com.maksimzotov.weatherhelper.databinding.CityFragmentBinding
 import com.maksimzotov.weatherhelper.presentation.main.ui.BaseFragment
 import com.maksimzotov.weatherhelper.presentation.ui.city.viewpager.ForecastAdapter
@@ -12,6 +17,11 @@ import com.maksimzotov.weatherhelper.presentation.ui.indicators.IndicatorsFragme
 class CityFragment : BaseFragment<CityFragmentBinding>(CityFragmentBinding::inflate) {
 
     private val viewModel by viewModels<CityViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,5 +44,14 @@ class CityFragment : BaseFragment<CityFragmentBinding>(CityFragmentBinding::infl
                 "Forecast"
             }
         }.attach()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.indicators_menu, menu)
+
+        menu.findItem(R.id.menu_indicators).setOnMenuItemClickListener {
+            Toast.makeText(requireContext(), "!!!", Toast.LENGTH_SHORT).show()
+            true
+        }
     }
 }
