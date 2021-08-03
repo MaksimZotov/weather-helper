bb3dbc3b36d2d46f661efcdf62fdb48f6a0be8b1
MaksimZotov <zotovmaksim1254@gmail.com>
2021-08-03 20:34:24 +0300

    removed an unnecessary fragment

--- app/src/main/java/com/maksimzotov/weatherhelper/presentation/ui/filters/FiltersViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.maksimzotov.weatherhelper.presentation.ui.filters
-
-import androidx.lifecycle.ViewModel
-
-class FiltersViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
