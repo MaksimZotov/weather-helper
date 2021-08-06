6bd95c08c2cd93994def3b1b4b66e54fc2efebb7
MaksimZotov <zotovmaksim1254@gmail.com>
2021-08-06 20:15:33 +0300

    cleanup

--- app/src/main/java/com/maksimzotov/weatherhelper/data/android/AndroidSettingsDatabase.kt
+++ app/src/main/java/com/maksimzotov/weatherhelper/data/android/AndroidSettingsDatabase.kt
@@ -1,8 +1,6 @@
 package com.maksimzotov.weatherhelper.data.android
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.maksimzotov.weatherhelper.presentation.entities.BottomNavigation
 import com.maksimzotov.weatherhelper.presentation.entities.DarkTheme
@@ -19,28 +17,4 @@ import com.maksimzotov.weatherhelper.presentation.entities.Temperature
 abstract class AndroidSettingsDatabase : RoomDatabase() {
 
     abstract fun androidSettingsDao(): AndroidSettingsDao
-
-    companion object {
-
-        val DATABASE = "database"
-
-        @Volatile
-        private var INSTANCE: AndroidSettingsDatabase? = null
-
-        fun getDatabase(context: Context) : AndroidSettingsDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AndroidSettingsDatabase::class.java,
-                    DATABASE
-                ).build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
 }
